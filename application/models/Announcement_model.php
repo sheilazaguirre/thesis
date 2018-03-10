b3f988ec98227714a54737b4622bd5376c7bdb52
sheilazaguirre <sheila.zaguirre@benilde.edu.ph>
2018-03-10 15:53:31 +0800

    Grades
    Working POST method lang kulang

--- application/models/Announcement_model.php
+++ application/models/Announcement_model.php
@@ -18,11 +18,7 @@ class Announcement_model extends CI_Model
         $query = $this->db->query(
             "SELECT a.announceID, CONCAT(u.userFN,' ', u.userLN) as userName, a.announceTitle, a.announceFile, a.announceDetail, a.announceDate, a.dateUploaded, a.dateModified, a.dateExpiry, a.status from announcements a
             INNER JOIN users u ON a.userID = u.userID
-<<<<<<< HEAD
-            WHERE a.status ='Active'"
-=======
             WHERE a.status='Active'"
->>>>>>> a0353c446111b681e373a7be49fff961473562f8
         );
         return $query->result_array();
     }
